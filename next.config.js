0abbbc5d463e6ecfe01ae4408ef0e5f32228b974
Kyle Durand <kyledurand@gmail.com>
2022-10-15 19:25:40 -0400

    graphql

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+module.exports = {
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.graphql$/,
+      exclude: /node_modules/,
+      use: [options.defaultLoaders.babel, { loader: "graphql-let/loader" }],
+    });
 
-module.exports = nextConfig
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      type: "json",
+      use: "yaml-loader",
+    });
+
+    return config;
+  },
+};
