d0ee2e53a1ab8d0bd7c70ff2a9efbaee2616c3a1
Kyle Durand <kyledurand@gmail.com>
2023-01-17 21:05:20 -0500

    Add footer pages and content

---
+++ pages/contact.tsx
@@ -0,0 +1,49 @@
+import { PortableText } from "@portabletext/react";
+import { GetStaticProps } from "next";
+import { createClient } from "next-sanity";
+import { Nav } from "components/shared/nav";
+import { Footer } from "components/shared/footer";
+import styled from "styled-components";
+
+interface Data {
+  _key: string;
+  content: any;
+}
+
+interface Props {
+  data: Data[];
+}
+
+export default function Contact({ data }: Props): React.ReactNode {
+  return (
+    <Container>
+      <Nav search="" setSearch={() => {}} />
+      <PortableText value={data[0].content} />
+      <Footer />
+    </Container>
+  );
+}
+
+const client = createClient({
+  projectId: "oldv6j45",
+  dataset: "production",
+  apiVersion: new Date().toISOString().split("T")[0],
+  useCdn: false,
+});
+
+export const getStaticProps: GetStaticProps = async function () {
+  const data = await client.fetch(`*[_type == "contact"]`);
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1080px;
+  margin: 0 auto;
+  background-color: var(--background);
+`;
